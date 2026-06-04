45bf298f21caf6315a066c251d5cc2abea2ec0ca
Rehcra867 <archer.temnoff587@schools.sa.edu.au>
2026-06-05 08:36:23 +0930

    Add socket.io login and registration functionality

---
+++ public/script.js
@@ -0,0 +1,63 @@
+const socket = io();
+
+const usernameInput =
+  document.getElementById("username");
+
+const passwordInput =
+  document.getElementById("password");
+
+const loginDiv =
+  document.getElementById("login");
+
+const lobbyDiv =
+  document.getElementById("lobby");
+
+const chipsSpan =
+  document.getElementById("chips");
+
+const messageDiv =
+  document.getElementById("message");
+
+function register() {
+
+  socket.emit("register", {
+    username: usernameInput.value,
+    password: passwordInput.value
+  });
+
+}
+
+function login() {
+
+  socket.emit("login", {
+    username: usernameInput.value,
+    password: passwordInput.value
+  });
+
+}
+
+socket.on("registerSuccess", () => {
+  messageDiv.innerText =
+    "Registration successful!";
+});
+
+socket.on("registerError", (msg) => {
+  messageDiv.innerText =
+    "Register Error: " + msg;
+});
+
+socket.on("loginError", (msg) => {
+  messageDiv.innerText =
+    "Login Error: " + msg;
+});
+
+socket.on("loginSuccess", (user) => {
+
+  loginDiv.classList.add("hidden");
+
+  lobbyDiv.classList.remove("hidden");
+
+  chipsSpan.innerText =
+    user.chips;
+
+});
